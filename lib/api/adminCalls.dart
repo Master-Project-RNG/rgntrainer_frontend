6b052f0a7b71a3d5c010272f2098b6eb5b908321
reffx <ripsli@hotmail.com>
2021-05-03 22:09:45 +0200

    #15: getTrainerStatus() & getResult implemented and tested on local and production

--- lib/api/adminCalls.dart
+++ lib/api/adminCalls.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
 import 'dart:html';
 
-import 'package:url_launcher/url_launcher.dart';
-
 import 'package:flutter/material.dart';
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
