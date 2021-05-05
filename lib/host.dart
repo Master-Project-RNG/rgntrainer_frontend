8cb4a52fae32aea643481668836208b41deb2a12
reffx <ripsli@hotmail.com>
2021-05-05 12:13:41 +0200

    #20: Host domain to https

--- lib/host.dart
+++ lib/host.dart
@@ -1,6 +1,6 @@
 class Host {
-  static const String localhost = "http://192.168.43.109:8080";
-  static const String production = "http://188.155.65.59:8081";
+  static const String localhost = "https://192.168.43.109:8080";
+  static const String production = "https://188.155.65.59:8081";
 
   static final Host _host = Host._internal();
 
