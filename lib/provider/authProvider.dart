c918ec5a0da54cef761a99034f45e0868051744b
Reffx <ripsli@hotmail.com>
2021-05-07 12:36:14 +0200

    #20: rollback

--- lib/provider/authProvider.dart
+++ lib/provider/authProvider.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:rgntrainer_frontend/host.dart';
@@ -8,11 +7,6 @@ import 'dart:convert';
 
 class AuthProvider with ChangeNotifier {
   var activeHost = Host().getActiveHost();
-  late User currentUser;
-
-  User get loggedInUser {
-    return currentUser;
-  }
 
   Future<void> _authenticate(String? username, String? password) async {
     final url = '${activeHost}/login';
@@ -30,22 +24,19 @@ class AuthProvider with ChangeNotifier {
       ).then(
         (response) {
           final Map<String, dynamic> responseData = json.decode(response.body);
-          currentUser = User(
+          final User newUser = User(
             username: responseData['username'].toString(),
-            token: responseData['token'].toString(),
-            usertype: responseData['usertype'].toString(),
+            password: responseData['password'].toString(),
           );
         },
       );
       /* if (responseData['error'] != null) {
         throw HttpException(responseData['error']['message']);
       } --> Dazu ist nichts in der DB */
-
     } catch (error) {
       throw error;
     }
   }
-
   Future<void> login(String? username, String? password) async {
     return _authenticate(username, password);
   }
