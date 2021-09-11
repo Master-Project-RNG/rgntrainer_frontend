d18a52f5328a2fb2f906cd80937b02aeadf02f58
DESKTOP-QIONQIP\ripsl <ripsli@hotmail.com>
2021-09-11 19:15:56 +0200

    #44: Refactoring according lint standarts

--- lib/screens/admin_numbers_screen.dart
+++ lib/screens/admin_numbers_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rgntrainer_frontend/models/numbers_model.dart';
